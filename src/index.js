abc93ad81664928c81c9306fb7e3c97209b7f9ee
espere-papillon <marinabov007@gmail.com>
2021-03-14 03:51:29 +0300

    init: add typical-arrays-problems-task

---
+++ src/index.js
@@ -0,0 +1,12 @@
+
+exports.min = function min (array) {
+  return (array !== undefined) ? (array.length !== 0) ? Math.min(...array) :0 : 0;
+}
+
+exports.max = function max (array) {
+  return (array !== undefined) ? (array.length !== 0) ? Math.max(...array) :0 : 0;
+}
+
+exports.avg = function avg (array) {
+  return (array !== undefined) ? (array.length !== 0) ? (array.reduce((a, b) => a + b, 0))/array.length : 0 : 0;
+}
